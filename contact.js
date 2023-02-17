4e5f17197e7f18153565fb781b7d064d6b44cb8c
Suresh Yadav <suresh.y.np@gmail.com>
2023-02-17 13:44:05 +0545

    success

---
+++ contact.js
@@ -0,0 +1,14 @@
+function sendMessage()
+{
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+console.log(name);
+console.log(email)
+console.log(subject)
+console.log(message)
+   
+
+
+}
