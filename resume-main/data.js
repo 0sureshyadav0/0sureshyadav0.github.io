0d5530e3d57668d4f4161788f6452c608ca15972
Suresh Yadav <117462201+0sureshyadav0@users.noreply.github.com>
2023-02-22 09:45:29 +0545

    Update data.js

--- resume-main/data.js
+++ resume-main/data.js
@@ -1,8 +1,7 @@
 const userDetails = {
     name: "Suresh Yadav",
     designation: "Full Stack Software Engineer",
-    // A multi-skilled and dedicated IT professional with over 5 years of experience, adept in software development, well-versed in different programming languages. Well-rounded experience in OOPs, mobile development and user-centered design. A prodigy in learning new technologies. Strong background in team management and delegation.
-    description: "A person who is more enthusiast in tech with an experience of two year in HTML, CSS, JS, PHP, MySQL, Flutter, Dart & C  as well as an experienced person in Android and iOS development. I can provide you with application & web development using Flutter framework. As I am mostly a self-learner, I can easily adapt to new things and always ready to take on a challenge. For more info visit: https://sureshyadav.info.np/",
+    description: "Computer Science undergraduate with 2+ years of experience in HTML, CSS, JS, PHP, MySQL, Flutter, Dart & C  as well as in Android and iOS development. I can provide you with application & web development using Flutter framework. As I am mostly a self-learner, I can easily adapt to new things and always ready to take on a challenge. For more info visit: https://sureshyadav.info.np/",
     picture: {
         src: "suresh.jpg",
         link: "https://www.linkedin.com/in/rajyadavnp"
@@ -59,13 +58,7 @@ const userDetails = {
 }
 
 const skills = [
-    // {
-    //     icon: "fab fa-android",
-    //     title: "Android",
-    //     scale: 5,
-    //     tech: ["Java", "Kotlin", "XML", "Gradle"],
-    //     lib: ["RxJava", "Room", "Dagger", "LiveData", "Retrofit", "Firebase"]
-    // },
+   
     {
         icon: "<div class='flutter-icon'></div>",
         title: "Flutter",
@@ -87,27 +80,7 @@ const skills = [
         tech: ["Flutter","Firebase"],
         lib: ["Alamofire", "SQLite.swift"]
     },
-    // {
-    //     icon: "fab fa-react",
-    //     title: "ReactJS",
-    //     scale: 5,
-    //     tech: ["JavaScript", "TypeScript", "npm"],
-    //     lib: ["Redux", "FCM", "AntD"]
-    // },
-    // {
-    //     icon: "fab fa-python",
-    //     title: "Python",
-    //     scale: 5,
-    //     tech: ["Python", "Django", "pip"],
-    //     lib: ["TensorFlow", "OpenCV", "celery", "geodjango", "channels"]
-    // },
-    // {
-    //     icon: "fab fa-golang",
-    //     title: "GoLang",
-    //     scale: 5,
-    //     tech: ["Go", "gRPC"],
-    //     lib: ["Gin", "Ginkgo"]
-    // },
+   
     {
         icon: "fa fa-globe",
         title: "Web",
@@ -129,13 +102,7 @@ const skills = [
         tech: ["Git"],
         lib: ["Mercurial"]
     },
-    // {
-    //     icon: "fa fa-server",
-    //     title: "Server & Scripting",
-    //     scale: 3,
-    //     tech: ["NGINX", "Shell"],
-    //     lib: ["Docker"]
-    // },
+  
     {
         icon: "fa fa-code",
         title: "Other Languages & Frameworks",
@@ -143,14 +110,7 @@ const skills = [
         tech: ["C", "C++","JAVA"],
         lib: ["ASP.NET", "Lucene Elastic Search", "Rust", "C#.NET"]
     },
-    // {
-    //     printBreak: true,
-    //     icon: "fab fa-app-store-ios",
-    //     title: "Applications",
-    //     scale: 4,
-    //     tech: ["Adobe Illustrator", "Adobe AfterEffects", "CorelDRAW", "Microsoft Office"],
-    //     lib: []
-    // }
+  
 ];
 
 
@@ -214,9 +174,7 @@ const personal = [
 ];
 
 const experiences = [
-    // {
-    //     update: "Will be published after having experience in TechCompany"
-    // }
+   
     {
         position: "Will be published after having experience in TechCompany.",
         company: "",
@@ -224,66 +182,11 @@ const experiences = [
         tech: [],
         achievements: [],
     },
-    // {
-    //     position: "Lead Engineer",
-    //     company: "ShopUp India, Bangalore",
-    //     duration: "Dec 2022 - Present",
-    //     tech: ["GoLang", "Flutter", "ReactJS"],
-    //     achievements: [],
-    // },
-    // {
-    //     position: "Senior Engineer",
-    //     company: "Falabella India, Bangalore",
-    //     duration: "Oct 2020 - Nov 2022",
-    //     tech: ["Flutter", "ReactJS"],
-    //     achievements: [
-    //         "Developed applications for Android and iOS using Flutter framework",
-    //         "Designed elegant and user-friendly interfaces for mobiles and web",
-    //         "Produced clean, efficient code based requirement specifications",
-    //         "Integrated software components and third-party APIs",
-    //         "Written unit and integration test cases",
-    //         "Troubleshot, fixed and/or improved existing software",
-    //         "Created technical documentation for reference and reporting",
-    //     ],
-    // },
-    // {
-    //     position: "Team Lead and Developer",
-    //     company: "Netzary Infodynamics",
-    //     duration: "Jul 2017 - Sep 2020",
-    //     tech: ["Android", "iOS", "Flutter", "Python", "Web"],
-    //     achievements: [
-    //         "Supervised and managed all the activities within the team",
-    //         "Identified team goal and evaluated their progress",
-    //         "Coached team members to develop necessary skills",
-    //         "Delegated tasks to individual team members comprehending their abilities",
-    //         "Helped the team accomplish the set goals",
-
-    //         "Developed applications for Android and iOS using native as well as hybrid tool (Flutter)",
-    //         "Developed applications for Web with Python on top of Django framework",
-    //         "Designed elegant and user-friendly interfaces for mobiles and web",
-    //         "Produced clean, efficient code based requirement specifications",
-    //         "Integrated software components and third-party APIs",
-    //         "Tested and deployed programs and/or systems",
-    //         "Troubleshot, fixed and/or improved existing software",
-    //         "Created technical documentation for reference and reporting",
-    //     ],
-    // },
+   
 ];
 
 const education = [
-    // {
-    //     board: "Bangalore University",
-    //     school: "RR Institutions/Bangalore, IN",
-    //     concentration: "Bachelor of Computer Application",
-    //     percentage: 86,
-    //     duration: "Apr 2014 - Jun 2017",
-    //     achievements: [
-    //         "Designed and developed an online University data processing system using ASP MVC, MS SQL Express, web technologies as an academic project",
-    //         "Designed and developed an SIMS Windows Form Application using C#.NET and MS SQL Express as an academic project",
-    //         "Created an Android application for cultural event held in the college with features like registration, information about events, etc.",
-    //         "Participated and won inter-college competition for web application development"
-    //     ]
-    // },
+   
     {
         board: "National Education Board (NEB)",
         school: "Gorkha Model Sec. School, Lamahi-Nepal",
@@ -337,24 +240,11 @@ const personalProjects = [
             {
                 icon: "fa fa-link",
                 tooltip: "Check it out",
-                url: "https://pub.dev/packages/safe_device_check/"
+                url: "#"
             }
         ]
     },
-    // {
-    //     name: "PopupMenu (not maintained)",
-    //     description: "Customizable android popup menu with support support for icons",
-    //     duration: "Sep 2018",
-    //     tech: ["Android", "Kotlin"],
-    //     refs: [
-    //         {
-    //             icon: "fa fa-link",
-    //             tooltip: "Check it out",
-    //             url: "https://github.com/rajyadavnp/PopupMenu"
-    //         }
-    //     ]
-    // },
-
+   
 
 ];
 
@@ -368,7 +258,7 @@ const companyProjects = [
             {
                 icon: "fa fa-link",
                 tooltip: "Check it out",
-                url: "https://pub.dev/packages/time_dropper/"
+                url: "#"
             }
         ]
     },
@@ -379,23 +269,6 @@ const companyProjects = [
         tech: ["Flutter","Dart"],
         refs: []
     },
-    // {
-    //     name: "Urdu Audio",
-    //     description: "Urdu audio is an audio streaming mobile application available on Android and iOS platform.",
-    //     duration: "Feb 2018 - Mar 2018",
-    //     tech: ["Flutter", "Scoped Model", "FCM"],
-    //     refs: [
-    //         {
-    //             icon: "fab fa-google-play",
-    //             tooltip: "View in play store",
-    //             url: "https://play.google.com/store/apps/details?id=com.bhatkallys.urduaudio",
-    //         },
-    //         {
-    //             icon: "fab fa-app-store",
-    //             tooltip: "View in app store",
-    //             url: "https://apps.apple.com/us/app/urdu-audio/id885622514",
-    //         },
-    //     ]
-    // },
+    
 ];
 
