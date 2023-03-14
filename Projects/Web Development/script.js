7a1a6e3608da2ce233cee6966ee49150ee4423d6
Suresh Yadav <suresh.y.np@gmail.com>
2023-03-14 19:14:56 +0545

    new commit

--- Projects/Web Development/script.js
+++ Projects/Web Development/script.js
@@ -3,15 +3,19 @@ document.getElementById("hamburger").addEventListener("click", showMenu);
 var isEnable = false;
 function showMenu() {
   if (isEnable == true) {
-    document.getElementById("hamburgerMenu").style = "display:none";
+   
+    document.getElementById("hamburgerMenu").style = "display:flex;background-color: white;transition:3s ease-in-out;";
     document.getElementById("close").style.display = "none";
     document.getElementById("menu").style.display = "flex";
+
     isEnable = false;
   } else {
+   
     document.getElementById("hamburgerMenu").style =
-      "display:flex;animation-name:showMenu;animation-duration:1s;animation-direction:reverse;";
+      "display:flex;background-color: rgb(98, 9, 187);transition:3s ease-in-out;";
     document.getElementById("menu").style.display = "none";
     document.getElementById("close").style.display = "flex";
     isEnable = true;
   }
+  // animation-name:showMenu;animation-duration:.5s;animation-direction:reverse;
 }
